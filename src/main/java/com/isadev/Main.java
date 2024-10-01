093e7637850b191664b8224327121d055885d541
Isabel <isabel.vi.ro.4@outlook.com>
2024-10-01 15:49:01 -0600

    cambio en el mensaje de bienvenida

--- src/main/java/com/isadev/Main.java
+++ src/main/java/com/isadev/Main.java
@@ -2,6 +2,7 @@ package com.isadev;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Welcome to javaBank ! new proyect");
     }
 }
